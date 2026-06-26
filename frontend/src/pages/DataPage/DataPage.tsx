7bf2f469fab823870c5208ce0fd7e01d3cf5de2f
mumn <mumn06n@gmail.com>
2026-06-26 17:20:45 +0900

    パスエイリアス設定、cssの作成、Headerの作成

--- frontend/src/pages/DataPage/DataPage.tsx
+++ frontend/src/pages/DataPage/DataPage.tsx
@@ -1,3 +1,26 @@
+import "./DataPage.css";
+import Header from "@/components/Header";
+
 export default function DataPage() {
-  return <h1>データ画面</h1>;
+  return (
+    <div className="data-page">
+
+      {/* タイトル */}
+      <header>
+        <Header title="データ" />
+      </header>
+
+      {/* 日付・時間 */}
+      <section className="date-area">
+        <h2>2026年</h2>
+      </section>
+
+      {/* センサから取得したデータ */}
+      <main className="data-area">
+        <p>気温</p>
+        <p>水温</p>
+      </main>
+
+    </div>
+  );
 }
